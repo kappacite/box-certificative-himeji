561889c7dc854814a1d75897bb16993faef1e87c
Stitchmod <tom2004.eisenschmidt@gmail.com>
2026-05-28 09:24:36 +0200

    Gestion Erreur direction + modif index

--- himeji-planner/src/router/index.js
+++ himeji-planner/src/router/index.js
@@ -1,10 +1,13 @@
 import Home from '@/views/Home.vue'
+import NotFound from '@/views/NotFound.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {path:'/', name:'Home', component:Home}
+    {path:'/', name:'Home', component:Home},
+    {path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound}
+
   ],
 })
 
---
+++ himeji-planner/src/views/NotFound.vue
@@ -0,0 +1,6 @@
+<script setup></script>
+
+<template>
+    404. ca va pas le chemin 
+    <router-link to="/">Retour à l'accueil </router-link>
+</template>

650e55d662f0cda600808239e28dd95d5c8f5460
Eythan Tournant <eythan.tournant@gmail.com>
2026-05-28 08:58:05 +0200

    Bases du projet
    Page d'accueil et création des dossiers

--- himeji-planner/src/router/index.js
+++ himeji-planner/src/router/index.js
@@ -1,8 +1,11 @@
+import Home from '@/views/Home.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {path:'/', name:'Home', component:Home}
+  ],
 })
 
 export default router
